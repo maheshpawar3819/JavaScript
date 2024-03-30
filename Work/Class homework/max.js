dd5191c4373441782809848e7e0dae503e58b0d9
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-03-30 13:03:05 +0530

    modify max.js file

--- Work/Class homework/max.js
+++ Work/Class homework/max.js
@@ -1,8 +1,15 @@
 // 1.write a programm in j/s to find maximum between three numbers.
 
-let num1 = 50;
+let num1 = 500;
 let num2 = 80;
-let num3 = 25;
+let num3 = 125;
 
-let max = Math.max(num1, num2, num3);
-console.log(max);
+if(num1>=num2 && num1>=num3){
+    console.log(`${num1} is the largest number`);
+}
+else if(num2>=num1 && num2>=num3){
+    console.log(`${num2} it the largest number`);
+}
+else{
+    console.log(`${num3} is the largest number`);
+}
