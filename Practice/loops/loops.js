716eed85686910e72e7e9a193043bab2acc133a7
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-01 19:17:20 +0530

    add more for loop question

--- Practice/loops/loops.js
+++ Practice/loops/loops.js
@@ -2,7 +2,15 @@
 
 //Print 1 to 10 numbers using for loop
 
-for(let i=1;i<=10;i++){
-    console.log(i);
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+//print all the number that are multiple of 5 with if condition
+
+for(let i=1;i<=100;i++){
+    if(i%5===0){
+        console.log(i);
+    }
 }
 
---
+++ Practice/ternaryexp.js
@@ -0,0 +1,11 @@
+//simple example conditional (ternary) operator.
+
+let num=4;
+(num>10)? console.log("numbers is larger than 10") : console.log("number is smaller than 10");
+
+
+//program to find number is odd or even with using conditional (ternary) operator.
+
+let number=9;
+
+(number%2===0) ? console.log(`${number} is a even number`) : console.log(`${number} is a odd number`);
