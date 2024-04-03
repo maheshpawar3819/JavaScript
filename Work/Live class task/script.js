ef6be4d1c2181023ce767488b794d7bd414f8cc3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-03 23:28:31 +0530

    add another example

--- Work/Live class task/script.js
+++ Work/Live class task/script.js
@@ -1,3 +1,14 @@
 for (let num = 0; num <= 30; num += 2) {
     console.log(num);
 }
+
+
+///with using if stement.
+
+console.log("2nd loop");
+
+for(let i=1;i<=20;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
