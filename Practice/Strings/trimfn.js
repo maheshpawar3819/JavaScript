670de27049d8d0db21f9ac03ac12e97b5910bff9
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-08 14:36:11 +0530

    add practice file trim() method

---
+++ Practice/Strings/trimfn.js
@@ -0,0 +1,24 @@
+//use case of trim function.
+//this trim() method used for trime the space form start and end of the string
+//string are immutable in j/s you can appy any string method on string but cannot change original string.
+
+let string="  mahesh  ";
+
+string.trim();
+console.log(string);
+ 
+
+/*
+let password =prompt("enter your password");
+console.log(password.trim()); 
+*/
+
+/*
+let username=prompt("enter your username");
+let password=prompt("enter you password");
+let result=username.trim();
+let result2=password.trim();
+
+console.log(result);
+console.log(result2);
+*/
