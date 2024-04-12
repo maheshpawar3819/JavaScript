38e5977ab2f8584e11653dbff6f6408f00fbc0dc
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-12 18:59:35 +0530

    add files

--- Practice/Starpatterns/str.js
+++ Practice/Starpatterns/str.js
@@ -1,14 +1,16 @@
-// simple star pattern 
+// simple star pattern
 
-let num=4;
+let num = 4;
 
-let str=""
+let str = "";
 
-for(let i=1;i<=num;i++){      //for row iteration
+for (let i = 1; i <= num; i++) {
+  //for row iteration
 
-    for(let j=1;j<=i;j++){        //for column iteration
-        str+="*";
-    }
-    str+="\n";
+  for (let j = 1; j <= i; j++) {
+    //for column iteration
+    str += "*";
+  }
+  str += "\n";
 }
 console.log(str);
