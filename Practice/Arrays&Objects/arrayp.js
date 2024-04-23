71cd75e080bbb261a2b6b366ec61689b2c9c45af
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-23 10:41:51 +0530

    add array practice file

---
+++ Practice/Arrays&Objects/arrayp.js
@@ -0,0 +1,23 @@
+//practice of arrays
+//array are mutable in js
+//in array you can store any time of value ex-string,number,boolen,null..
+//in array you can store diffrend values at in same array.
+
+let array = ["mahi", "kiran", 1, 2, 3, true, null];
+
+// console.log(array);
+
+//access elements of an array
+
+// console.log(array[1][0]);
+// console.log(array[array.length-1]);
+// console.log(typeof(array));
+
+//change elements in array
+
+array[1] = "sanket";
+array[2] = "rohit";
+array[3] = "rahul";
+array[4] = "ashwini";
+
+console.log(array);
