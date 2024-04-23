29a54507e1b38a9b6967192502ebd77720981027
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-23 10:11:19 +0530

    modifiy file checkprime.js

--- Practice/Arrays&Objects/checkprimen.js
+++ Practice/Arrays&Objects/checkprimen.js
@@ -3,7 +3,29 @@ let array=[1,2,3,4,5,6,7,8,9,10,11,12];
 let prime = [];
 let notprime = [];
 
+function checkprime(arr){
+  for(let i=0;i<arr.length;i++){
+     let isprime=true;
+    for(let j=2;j<=Math.sqrt(arr[i]);j++){
+      if(arr[i]%j===0){
+        notprime.push(arr[i]);
+        isprime=false;
+        break;
+      }
+    }
+    if(isprime !==false){
+      prime.push(arr[i]);
+    }
+  }
+ 
+  console.log("prime numbers",prime);
+  console.log("not prime numbers",notprime);
+}
 
+checkprime([1,2,3,4,5,6,7,8,9,10,11,12,13]);
+
+
+/*
 for(let i=0;i<array.length;i++){
     let isprime=true;
     for(let j=2;j<=Math.sqrt(array[i]);j++){
@@ -21,24 +43,8 @@ console.log("prime",prime);
 console.log("not prime",notprime);
 
 
-
-
-/*
-function checkprime(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 2; j <= Math.sqrt(arr[i]); j++) {
-      if (arr[i] % j === 0) {
-        notprime.push(arr[i]);
-        isprime = false;
-        break;
-      }
-    }
-    if (isprime !== false) {
-      prime.push(arr[i]);
-    }
-  }
-  console.log("prime numbers", prime);
-  console.log("not prime numbers", notprime);
-}
-
 */
+
+
+
+
