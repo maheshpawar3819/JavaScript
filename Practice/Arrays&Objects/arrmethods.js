9f363593705b385af569c87ace3298fd40b2ca15
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-24 10:08:43 +0530

    add array methods practice file

---
+++ Practice/Arrays&Objects/arrmethods.js
@@ -0,0 +1,25 @@
+//practice of push,pop,shift,unshift
+
+let arr=["balram","swapnil","mahesh","kiran"];
+
+//remove balram and swapnil and add others to replace them.
+//using shift unshift
+
+arr.shift();
+arr.shift();
+
+arr.unshift("ashwini","neha");
+// arr.unshift("neha");
+console.log(arr);
+
+
+//remove last two 
+//use and add new one
+//using push pop 
+
+arr.pop();
+arr.pop();
+
+//add new 
+arr.push("mrunal","pratiksha")
+console.log(arr);
