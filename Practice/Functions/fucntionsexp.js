536cddaf42bdf964e47495395702c5cd8b632956
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-26 09:26:50 +0530

    add fn exp

---
+++ Practice/Functions/fucntionsexp.js
@@ -0,0 +1,15 @@
+//function expressions
+//the function can store in the variable and you can use any where by the variable name
+
+let sum=function(a,b){
+    return a+b;
+}
+
+// console.log(sum(10,20));
+
+let sub=function(n1,n2){
+    return n1-n2;
+}
+
+// console.log(sub(20,10));
+console.log(sum(10,10)+sub(20,10));
