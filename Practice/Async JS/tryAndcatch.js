7ddb4d2490191b1b712c4629d80f7e7b3b7d9ae7
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-04-28 10:54:40 +0530

    add practice files

---
+++ Practice/Async JS/tryAndcatch.js
@@ -0,0 +1,14 @@
+// Try ==> The Try Statement Allows you define a block of code to be tested for errors while it is being executed.
+// Catch ==> The Catch Statement Allows you define a block of code to be executed if the error occurs in the try block.
+
+console.log("hii mahi");
+
+try{
+    console.log(a);
+}catch{
+    console.log("a was not define")
+}
+
+console.log("hii pratik");
+
+console.log("hii shri");
