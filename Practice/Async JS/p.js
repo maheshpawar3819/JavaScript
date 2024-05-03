f7de774ea4bf6ceece62b2f2ddc1f78e07043255
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-03 10:44:16 +0530

    add files

---
+++ Practice/Async JS/p.js
@@ -0,0 +1,13 @@
+// Given an array A of positive integers.Your task is to find the leaders in the array.An element of array is a leader if it is greater than or equal to all the elements to its right side.The rightmost element is always a leader.
+
+// input = [16, 17, 4, 3, 5, 2]
+// output = [17, 5, 2]
+
+let array=[10,20,15,11,25];
+
+function findleader(array){
+    for(let i=0;i<array.length;i++){
+        
+    }
+}
+
