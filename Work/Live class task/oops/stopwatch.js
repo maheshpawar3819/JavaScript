a6a7c3ea906718338d00c0066e81f24b3fcbd605
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-04 21:31:25 +0530

    add stop watch file modify

--- Work/Live class task/oops/stopwatch.js
+++ Work/Live class task/oops/stopwatch.js
@@ -1,3 +1,46 @@
+// start function, stop function, duration in seconds -> 3.512 s
+function Stopwatch() {
+  let startTime, endTime, running, duration = 0;
+ 
+  this.start = function() {
+    if(running) {
+      throw new Error('Stopwatch is already running!');
+    }
+ 
+    running = true;
+ 
+    startTime = new Date();
+  }
+ 
+  this.stop = function() {
+    if(!running) {
+      throw new Error('Stopwatch is not yet running');
+    }
+ 
+    running = false;
+ 
+    endTime = new Date();
+ 
+    const seconds = (endTime.getTime() - startTime.getTime())/1000;
+ 
+    duration = duration + seconds;
+  }
+ 
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  }
+ 
+  Object.defineProperty(this, 'duration', {
+    get: function() {return duration}
+  })
+}
+ 
+let sw = new Stopwatch();
+
+/*
 function Stopwatch() {
   let start,
     end,
@@ -29,3 +72,5 @@ setTimeout(() => {
   stpwatch.stop();
   console.log("Duration:", stpwatch.duration);
 }, 3000);
+
+*/
