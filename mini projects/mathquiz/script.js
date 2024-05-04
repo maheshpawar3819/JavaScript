19ba348e0da0c5764cf67ccab92e6abe078ed71d
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-04 12:10:33 +0530

    pendign for output

--- mini projects/mathquiz/script.js
+++ mini projects/mathquiz/script.js
@@ -1,26 +1,87 @@
-function generatequestions() {
-  const num1 = Math.floor(Math.random() * 10) + 1;
-  const num2 = Math.floor(Math.random() * 10) + 1;
-  const operation = ["+", "-", "*", "/"][Math.floor(Math.random() * 4)];
-  let answer;
-  let questionstext;
+let totalQuestions = 0;
+let correctAnswers = 0;
+let num1, num2, operation, answer;
+
+function generateQuestion() {
+  num1 = Math.floor(Math.random() * 10) + 1;
+  num2 = Math.floor(Math.random() * 10) + 1;
+  operation = ["+", "-", "*", "/"][Math.floor(Math.random() * 4)];
+
+  switch (operation) {
+    case "+":
+      answer = num1 + num2;
+      break;
+    case "-":
+      answer = num1 - num2;
+      break;
+    case "*":
+      answer = num1 * num2;
+      break;
+    case "/":
+      answer = num1 / num2; // You might want to handle division by zero
+      break;
+    default:
+      answer = num1 + num2; // Default to addition
+  }
+
+  // Generate the question text
+  const questionText = `${num1} ${operation} ${num2}?`;
+  document.getElementById("question").innerText = questionText;
 }
 
-function nextquestion() {
+function nextQuestion() {
   document.getElementById("answer").value = "";
-  generatequestions();
-  displayresult();
+  generateQuestion();
+  document.getElementById("result").innerText = "";
 }
 
-function displayresult() {
-  if (totalquestions > 0) {
-    const score = ((correctans / totalquestions) * 100).toFixed(2);
+function checkAnswer() {
+  const userAnswer = parseInt(document.getElementById("answer").value);
+  if (userAnswer === answer) {
+    correctAnswers++;
+    document.getElementById("result").innerText = "Correct!";
+  } else {
+    document.getElementById("result").innerText = "Incorrect!";
+  }
+  totalQuestions++;
+  displayResult();
+}
+
+function displayResult() {
+  if (totalQuestions > 0) {
+    const score = ((correctAnswers / totalQuestions) * 100).toFixed(2);
     document.getElementById(
       "result"
-    ).innerText = `score: ${score}% (${correctans}/${totalquestions})`;
-  }else{
-    document.getElementById('result').innerText='';
+    ).innerText += ` Score: ${score}% (${correctAnswers}/${totalQuestions})`;
   }
 }
-generatequestions();
-displayresult();
+
+// Initial setup
+generateQuestion();
+
+// function generatequestions() {
+//   const num1 = Math.floor(Math.random() * 10) + 1;
+//   const num2 = Math.floor(Math.random() * 10) + 1;
+//   const operation = ["+", "-", "*", "/"][Math.floor(Math.random() * 4)];
+//   let answer;
+//   let questionstext;
+// }
+
+// function nextquestion() {
+//   document.getElementById("answer").value = "";
+//   generatequestions();
+//   displayresult();
+// }
+
+// function displayresult() {
+//   if (totalquestions > 0) {
+//     const score = ((correctans / totalquestions) * 100).toFixed(2);
+//     document.getElementById(
+//       "result"
+//     ).innerText = `score: ${score}% (${correctans}/${totalquestions})`;
+//   }else{
+//     document.getElementById('result').innerText='';
+//   }
+// }
+// generatequestions();
+// displayresult();
