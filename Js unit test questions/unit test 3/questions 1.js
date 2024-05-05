f150abad48bb79f838af29a58be57231671d1be3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-05 19:49:15 +0530

    add folder unit test

---
+++ Js unit test questions/unit test 3/questions 1.js
@@ -0,0 +1,6 @@
+// Write a function which takes two arguments - an array and a value. If the value provided in the argument is found in the array, the function should return the position (index) of the element in an array. If the element is not found, the function should return -1.
+// example:
+// input:[1,2,3,4,5],2
+// output:1
+// input:[10,23,46,81,100],34
+// output:-1
