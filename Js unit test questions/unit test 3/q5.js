a32789e359aec83dfda092e6496c3db7a1322fcf
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-06 11:12:45 +0530

    add q5 in unit test 3

---
+++ Js unit test questions/unit test 3/q5.js
@@ -0,0 +1,10 @@
+// Write a program that will take a number as argument and return the square of that number.
+// example: input:3 output:9
+// input:5 output:25
+
+function square(num){
+    return num*num;
+}
+console.log(square(2));
+console.log(square(10));
+console.log(square(3));
