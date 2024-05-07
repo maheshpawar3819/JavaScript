a02ce376e7393fb7082f02e7d2f59b45d8f01928
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-07 12:54:53 +0530

    add squareroot practice file

---
+++ Practice/Arrays&Objects/map/squareroot.js
@@ -0,0 +1,8 @@
+// Calculate Square Roots: of every Element in of the array using map method
+
+let arr=[2,4,6,8,10];
+
+let squreroot=arr.map((ele)=>{
+    return Math.sqrt(ele);
+})
+console.log(squreroot);
