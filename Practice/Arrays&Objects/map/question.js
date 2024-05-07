8a7e4b6c4665a0983350b287a04e0d16163ee8f1
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-07 12:31:42 +0530

    add map practice question

---
+++ Practice/Arrays&Objects/map/question.js
@@ -0,0 +1,9 @@
+// Qs2. Create a new array using the map function whose each element is equal to the
+// original element plus 5.
+
+let array=[2,4,6,8,-2,-4];
+
+let result=array.map((ele)=> {
+    return ele+5;
+})
+console.log(result);
