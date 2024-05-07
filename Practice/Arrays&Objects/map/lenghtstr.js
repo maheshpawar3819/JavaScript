1b4324a3f5e203a519a88e43f1e38f13de9f6b2d
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-07 12:57:30 +0530

    add lengthstr.js file

---
+++ Practice/Arrays&Objects/map/lenghtstr.js
@@ -0,0 +1,8 @@
+// Extract Lengths of Strings: using map() method
+
+let str=["mahesh","kiran","sanket","rahul","ashwini","neha","vaishnavi","pratiksha"];
+
+let length=str.map((ele)=>{
+    return ele.length;
+})
+console.log(length);
