c3a61b7b794604c963d57f55f64df30378435871
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-09 17:19:57 +0530

    practice

--- Practice/febnums.js
+++ Practice/febnums.js
@@ -1,5 +1,18 @@
 //print febonacci numbers 
 
+function febnum(num){
+    let n1=0,n2=1,nextnum;
+    for(let i=1;i<num;i++){
+        console.log(n1);
+        nextnum=n1+n2;
+        n1=n2;
+        n2=nextnum;
+    }
+}
+febnum(10);
+
+
+/*
 function feb(number){
     let n1=0,n2=1,nextnum;
     for(let i=1;i<=number;i++){
@@ -10,3 +23,4 @@ function feb(number){
     }
 }
 feb(10);
+*/
