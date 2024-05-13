1797a89a54a25cdbbdfb284ae3f77e65e26800b9
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-13 15:21:52 +0530

    add unit test 4 practical question 1st

---
+++ Js unit test questions/unit test 4/q1.js
@@ -0,0 +1,15 @@
+// Write a function to reverse an array.
+// Examples: input:[1, 2, 3, 4] output [4, 3, 2, 1]
+// input :[10,12,14,16,18] output: [18,16,14,12,10]
+
+// let array=[1,2,3,4];
+// array.reverse()
+// console.log(array);
+
+let array=[1,2,3,5];
+
+let rev=(arr) => {
+    return arr.reverse();
+}
+
+console.log(rev(array));
