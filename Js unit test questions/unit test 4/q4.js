2e15f48406bbe30bb2b488e1a0d0bccfc84e2c51
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-13 15:54:11 +0530

    add q5.js

---
+++ Js unit test questions/unit test 4/q4.js
@@ -0,0 +1,10 @@
+// Find the last element of an Array
+// Example: Input:[10,20,30] output:30 Input:["Ram","Rahim","Komal"] output: Komal
+
+let arr=['vaibhavi','apurva','pratiksha','ashwini'];
+// let ele=arr[arr.length-1]
+// console.log(ele);
+function last(arr){
+    return arr[arr.length-1];
+}
+console.log(last(arr));
