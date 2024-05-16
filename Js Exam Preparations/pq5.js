fc5faf041a604400513332d355e7616bff16883e
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-16 22:28:59 +0530

    add find args length file

---
+++ Js Exam Preparations/pq5.js
@@ -0,0 +1,10 @@
+// Create a function that returns the number of arguments it was called with.
+// example:
+// Input : "foo"
+// Output : 1
+
+function findargs(){
+    return arguments.length;
+}
+
+console.log(findargs('ashwini','mahesh','vaishnavi','jay'));
