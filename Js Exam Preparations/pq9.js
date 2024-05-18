3f23f3917bfe736db528e6775f30377b75d0dca1
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-18 08:13:32 +0530

    add another way to find palendrome string

--- Js Exam Preparations/pq9.js
+++ Js Exam Preparations/pq9.js
@@ -5,3 +5,19 @@ const palendrome= (str)  => {
     return str===revstr;
 }
 console.log(palendrome('madam'));
+// console.log(palendrome('racecar'));
+// console.log(palendrome('laptop'));
+
+//with using if condition
+
+const checkpalendrome=(str) => {
+    let rev=str.split('').reverse().join('');
+    if(rev===str){
+        return ('given string is palendrome');
+    }else{
+        return ('given string is not a palendrome')
+    }
+}
+
+// console.log(checkpalendrome("mahesh"));
+console.log(checkpalendrome("racecar"));
