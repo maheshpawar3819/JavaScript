99ed000dba5ee21676045fe023f11191a5d8bbae
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-18 07:52:19 +0530

    add file to delete last element from the array

---
+++ Js Exam Preparations/pq7.js
@@ -0,0 +1,9 @@
+//write a function to delete last item in the array.
+
+let array=['mahesh','soham','kiran','suraj','sanket'];
+
+let lastele=(array) => {
+    return array.pop();
+}
+
+console.log(lastele(array));
