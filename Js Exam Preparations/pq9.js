6aa0aa68da1e9d960ca177a294430255e6c40978
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-18 08:16:20 +0530

    modify the format

--- Js Exam Preparations/pq9.js
+++ Js Exam Preparations/pq9.js
@@ -1,23 +1,23 @@
 //write a j/s program to check string is palendrome or not
 
-const palendrome= (str)  => {
-    let revstr=str.split('').reverse().join('');
-    return str===revstr;
-}
-console.log(palendrome('madam'));
+const palendrome = (str) => {
+  let revstr = str.split("").reverse().join("");
+  return str === revstr;
+};
+console.log(palendrome("madam"));
 // console.log(palendrome('racecar'));
 // console.log(palendrome('laptop'));
 
 //with using if condition
 
-const checkpalendrome=(str) => {
-    let rev=str.split('').reverse().join('');
-    if(rev===str){
-        return ('given string is palendrome');
-    }else{
-        return ('given string is not a palendrome')
-    }
-}
+const checkpalendrome = (str) => {
+  let rev = str.split("").reverse().join("");
+  if (rev === str) {
+    return "given string is palendrome";
+  } else {
+    return "given string is not a palendrome";
+  }
+};
 
 // console.log(checkpalendrome("mahesh"));
 console.log(checkpalendrome("racecar"));
