d48bc3c0fb7c51d91c278c4dce4e2f2fcf6f116e
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-18 08:03:50 +0530

    add program to reverse string

--- Js Exam Preparations/pq7.js
+++ Js Exam Preparations/pq7.js
@@ -1,17 +1,17 @@
 //write a function to delete last item in the array.
 
-let array=['mahesh','soham','kiran','suraj','sanket'];
+let array = ["mahesh", "soham", "kiran", "suraj", "sanket"];
 
-let lastele=(array) => {
-    return array.pop();
-}
+let lastele = (array) => {
+  return array.pop();
+};
 
 console.log(lastele(array));
 
 //Delete first element from the array
 
-let firstele=(array) => {
-    return array.shift();
-}
+let firstele = (array) => {
+  return array.shift();
+};
 
 console.log(firstele(array));
---
+++ Js Exam Preparations/pq8.js
@@ -0,0 +1,6 @@
+// Write a function to reverse a string.
+
+const reversestr=(str) => {
+    return str.split('').reverse().join();
+}
+console.log(reversestr('mahesh'));
