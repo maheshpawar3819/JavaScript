fdbe5d528a24feaf90f438f81b722cbb1cca9e13
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-19 16:04:15 +0530

    add concat method

---
+++ Js Exam Preparations/pq13.js
@@ -0,0 +1,12 @@
+// Merge two array
+
+// Example : Input: a= [1,2,3] b=[4,5,6] Output:[1,2,3,4,5,6]
+
+// Input: a= ["apple","mango","banana"] b=["orange","pine-apple"] Output:["apple","mango","banana","orange","pine-apple"]
+
+function con(arr1,arr2){
+    return arr1.concat(arr2);
+
+}
+
+console.log(con(['mahi','sanket'],['ashwini','apurva']));
