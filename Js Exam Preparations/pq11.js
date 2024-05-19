efb66343f0633e324767283369f6c980f839f593
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-19 15:42:52 +0530

    add simple star pattern question

---
+++ Js Exam Preparations/pq11.js
@@ -0,0 +1,13 @@
+//Write a program to print a pattern of stars using a nested for loop.
+
+function str(num) {
+  let star = "";
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      star += "*";
+    }
+    star += "\n";
+  }
+   return star;
+}
+console.log(str(5));
