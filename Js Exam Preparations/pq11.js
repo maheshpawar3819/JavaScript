df2ca5cfb26c9ce8c9d0d67066d29b93cc3e09b6
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-19 15:52:48 +0530

    modify

--- Js Exam Preparations/pq11.js
+++ Js Exam Preparations/pq11.js
@@ -3,11 +3,11 @@
 function str(num) {
   let star = "";
   for (let i = 1; i <= num; i++) {
-    for (let j = 1; j <= i; j++) {
+    for (let j = 1; j <= i  ; j++) {
       star += "*";
     }
     star += "\n";
   }
    return star;
 }
-console.log(str(5));
+console.log(str(2));
