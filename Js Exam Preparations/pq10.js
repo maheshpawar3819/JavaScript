00db409d96dc84ce6df72e2abeb57f7dcf746a68
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-19 15:29:34 +0530

    add the queston

---
+++ Js Exam Preparations/pq10.js
@@ -0,0 +1,9 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false. An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+function powerof(n){
+    if(n<=0){
+        return false;
+    }
+    return (n&(n-1))===0;
+}
+console.log(powerof(3));
