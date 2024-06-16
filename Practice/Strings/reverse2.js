ee2c0933f0018129899d1e9e564c24f7ee59ec58
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-16 18:53:15 +0530

    add the file

---
+++ Practice/Strings/reverse2.js
@@ -0,0 +1,15 @@
+//reverse the string without using loop
+let string="racecar";
+function rev(stirng) {
+
+    let reversestr=stirng.split('').reverse().join();
+    return reversestr;
+}
+
+if(rev("rececar")===string){
+    console.log('given string is palendrome str');
+}else{
+    console.log('given string is not a palendrome string');
+}
+
+// console.log(rev("mahesh"));
