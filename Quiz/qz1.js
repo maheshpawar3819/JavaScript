24fd696a3f7efa605e285cb41b0abf51a4161214
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-17 13:21:25 +0530

    add the quiz question

---
+++ Quiz/qz1.js
@@ -0,0 +1,11 @@
+function sum(nums){
+    var result=0;
+
+    for(var i=0;i<=nums.length;i++){
+        result +=nums[i];
+    }
+    return result;
+}
+
+var numbers=[1,2,3,4,5];
+console.log(`result : ${sum(numbers)}`);
