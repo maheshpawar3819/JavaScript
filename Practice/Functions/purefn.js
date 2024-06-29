fe9fe10433630c06fd76c9053f2df62346dc4042
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-29 12:51:42 +0530

    add im pure function

--- Practice/Functions/purefn.js
+++ Practice/Functions/purefn.js
@@ -1,9 +1,21 @@
-// in this file practice of pure and impure function 
+// in this file practice of pure and impure function
 
 // pure function:
-
-function pure(a,b){
-    return a+b;
+//pure functions is a function that always produce some output for the same input
+function pure(a, b) {
+  return a + b;
 }
 
-console.log(pure(3,4));
+console.log(pure(3, 4));
+
+//impure function:
+
+let count = 0;
+function impure(a) {
+ return count += a;
+}
+
+// it will changes output when we will called function multiple times
+//it can produce diffrent outputs form same inputs
+console.log(impure(2));
+console.log(impure(2));
