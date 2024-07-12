cdd99379672be3b092baefee0ef444d8e1fa4faa
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-12 19:34:07 +0530

    practice of hastag

---
+++ Interview preperation/Hashtaggenerator.js
@@ -0,0 +1,37 @@
+// Question : Hash tag generator
+
+/*
+
+-you are required to implement a function generateHash that generates a hash tag from a given input string the hash tag should be constructed as follows:
+
+-The input string should be converted to a has tag format, whare each word is capitalized and concatenated together with spaces.
+
+-if the length of the input string is greater than 280 characters or if the input string is impty or contains only whitespace , the fucntion should return false.
+
+-Otherwise, the function should return the generated hash tag prefixed with #
+
+input: "my name is mahesh pawar"
+output : "#MyNameIsMaheshPawar"
+
+*/
+
+let string = "my name is sanket pawar";
+
+let addhashtag = (str) => {
+  //check string length and blank string
+  if (str.length > 280 || str.trim().length === 0) {
+    return false;
+  }
+
+  //convert this string in to array
+  str = str.split(" ");
+  str = str.map((ele) => {
+    return ele.replace(ele[0], ele[0].toUpperCase());
+  });
+
+  //again this string array convert in strings
+  return (str = `#${str.join("")}`);
+};
+
+const finalresult = addhashtag(string);
+console.log(finalresult);
