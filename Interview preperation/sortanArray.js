17476a8ff1254a6ec3b1e529bc4f1f2e90079e3c
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-16 19:30:50 +0530

    add array sort question soultion 1 remaining another solution

---
+++ Interview preperation/sortanArray.js
@@ -0,0 +1,23 @@
+// Question : Sort an Array
+
+//write a fucntion to sort an array of the numbers in an assending order.
+//ex : console.log(sortAscending([5,3,1,8])); output:[1,3,5,8];
+//   : console.log(sortAscending([5,3,10,8])); output:[3,5,8,10];
+
+/*
+ To do requirements:
+                -the function should take array of numbers as an input 
+                -it should return new array with the numbers sorted in ascending order
+                -the original array should remain unchanged
+                -you are Not allow to use builtin sort() method
+
+*/
+
+let array = [2, 1, 4, 3, 9, 10, 12];
+
+const sortAscending = (arr) => {
+  //use compare function
+  return arr.sort((a, b) => a - b);
+};
+
+console.log(sortAscending(array));
