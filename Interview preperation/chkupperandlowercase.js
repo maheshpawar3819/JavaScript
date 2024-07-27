791b6b378d3bfdbd4931fe99342292bea7600254
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-27 19:22:38 +0530

    Day 18 write a function to check string uppercase or lowercase add another solution

--- Interview preperation/chkupperandlowercase.js
+++ Interview preperation/chkupperandlowercase.js
@@ -20,13 +20,28 @@ const Checkuppercase = (char) => {
 console.log(Checkuppercase("A"));
 console.log(Checkuppercase("m"));
 
-const Checklowercas=(char) => {
-    if(char.charCodeAt(0) >=97 && char.charCodeAt(0) <=122){
-        return true;
-    }
-    return false;
-} 
+const Checklowercas = (char) => {
+  if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+    return true;
+  }
+  return false;
+};
 
-console.log("CheckLowerCase",Checklowercas("A"));
-console.log("CheckLowerCase",Checklowercas("m"));
+console.log("CheckLowerCase", Checklowercas("A"));
+console.log("CheckLowerCase", Checklowercas("m"));
 
+// Solution 2 :
+
+const CheckUpper = (char) => {
+  return char === char.toUpperCase();
+};
+
+console.log("S2 checkuppercase", CheckUpper("A"));
+console.log("S2 checkuppercase", CheckUpper("m"));
+
+const CheckLower = (char) => {
+  return char === char.toLowerCase();
+};
+
+console.log("S2 checklowercase", CheckLower("A"));
+console.log("S2 checklowercase", CheckLower("m"));
