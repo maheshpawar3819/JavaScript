cb161aef47eac61fa098ec9d4dd216514a9a8028
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-01 18:55:40 +0530

    add another method to finding max num in the arr using for loop

--- Interview preperation/Arrays/Findmaxnum.js
+++ Interview preperation/Arrays/Findmaxnum.js
@@ -1,8 +1,28 @@
 // Question: find maximum number in the array
 
+/*
+// Solution 1:
 const findmaxn=(num)=> {
     let sp=Math.max(...num);
     return sp;
 }
 
 console.log(findmaxn([1,2,36,4,89,1000]));
+
+*/
+
+//Solution 2: for using for loop
+
+const findmaxn = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return console.log(max);
+};
+
+findmaxn([2, 42, 1, 4]);
+findmaxn([2, 42, 1, 5,100,30,4000]);
+findmaxn([1,2,36,4,89,1000]);
