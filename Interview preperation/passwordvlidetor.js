494b0fc72d180b7cafd102bc074cf5348074d0fa
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-09 19:06:28 +0530

    add another solution but simple password checker

--- Interview preperation/passwordvlidetor.js
+++ Interview preperation/passwordvlidetor.js
@@ -14,6 +14,9 @@ Output : true if the password meets all criteria
          Otherwise false.
 */
 
+// Solution 1 : with using regular expression
+
+/*
 const checkPassword = (password) => {
   let checkPassword =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
@@ -21,3 +24,31 @@ const checkPassword = (password) => {
 };
 
 console.log(checkPassword("Mahi8878"));
+
+*/
+
+// Solution 2 : without using regular expression simple password validetor
+
+const checkPassword2 = (password) => {
+  let hasLowercase = false;
+  let hasUppercase = false;
+  let hasNumber = false;
+
+  for (let char of password) {
+    if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+      hasUppercase = true;
+    } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+      hasLowercase = true;
+    } else if (!isNaN(Number(char))) {
+      hasNumber = true;
+    }
+  }
+
+  if (!hasLowercase || !hasUppercase || !hasNumber || password.length < 8) {
+    return false;
+  }
+
+  return true;
+};
+
+console.log(checkPassword2("afkdsdaee1A"));
