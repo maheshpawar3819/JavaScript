4097b5ad5b56489d5a5e03eff7745eb753773d42
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-12 20:22:31 +0530

    Day 35 check given obj is empty or not learn new property hasOwnProperty

---
+++ Interview preperation/Objects/checkobjemptyness.js
@@ -0,0 +1,19 @@
+// Question : Check Object Emptyness
+
+//write a function that takes object as an input and determines whether it empty or not. An empty object is defined as an object with no own properties.
+
+//Task impliment the function usign javascript and return a message indicating wether the object is empty or not;
+
+// Solution 1: 
+
+const checkObjempty=(obj) => {
+  for(let key in obj){
+    if(obj.hasOwnProperty(key)){
+        return `given object is not a empty`
+    }
+  }
+  return `given object is empty`
+}
+
+console.log(checkObjempty({name:"mahesh",age:20}));
+console.log(checkObjempty({}));
