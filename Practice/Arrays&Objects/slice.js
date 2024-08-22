db370c9cf5b63aec444a6c70e358b31b6f78a294
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-22 20:16:15 +0530

    add practice files

---
+++ Practice/Arrays&Objects/slice.js
@@ -0,0 +1,10 @@
+//Slice method pracitce
+
+let students=["Mahesh","Ashwini","Sneha","Kiran","Rohan","Kunal","Krish"];
+
+let cut=students.slice(3,5);
+
+console.log(cut);
+// console.log(students)
+
+
---
+++ Practice/Arrays&Objects/splice.js
@@ -0,0 +1,8 @@
+//Splice :if you want to add some elements in anywhere of an array then splice() method will usefull;
+//used to delete existig value.
+//it will not create new array it changes in existing array.
+
+let students=["Mahesh","Ashwini","Sneha","Kiran","Rohan","Kunal","Krish"];
+
+students.splice(2,1,"Mahi","Neha");
+console.log(students)
