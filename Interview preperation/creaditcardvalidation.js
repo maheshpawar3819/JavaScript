f85af91a4467a97f76a38e451d091875763575fc
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-23 21:25:11 +0530

    Day 42 Question validate a credit card number (luhn algorithm)

---
+++ Interview preperation/creaditcardvalidation.js
@@ -0,0 +1,9 @@
+//Question : Validate a Credit Card Number (Luhn Algorithm);
+/*
+Write a function named validateCareditCard that takes a credit card number as a string.
+
+Requirements:
+            -Write a function named validateCreditCard that takes a credit card number as a string.
+            -the function should retrun true if the number is valid according to the Luhn algorithm, and false otherwise.
+            -Ensure the function can handle numbers as strings,which may include spaces and dashes.
+*/
