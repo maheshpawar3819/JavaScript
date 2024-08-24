a85de4ee4979b11068a0161f0f8e6ab2fe5fa2e3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-24 18:26:45 +0530

    solve question

--- Interview preperation/fizzbuzz.js
+++ Interview preperation/fizzbuzz.js
@@ -9,3 +9,20 @@ For each number i in the range from startNum to endNum (both inclusive)
 -if i is divisible by only 5, include "buzz" in the result.
 -otherwiese, include the number i itself.
 */
+
+const fizzBuzz = (num1, num2) => {
+  let result = [];
+  for (let i = num1; i <= num2; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
+console.log(fizzBuzz(1, 5));
