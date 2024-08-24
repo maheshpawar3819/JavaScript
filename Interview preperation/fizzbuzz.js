2426c6c9871973510e4c8aeec303934296d3d547
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-24 18:10:36 +0530

    Day 43 add question imp question fizzbuzz

---
+++ Interview preperation/fizzbuzz.js
@@ -0,0 +1,11 @@
+//Question : Fizzbuzz challenge
+
+/*
+write a function that accept two numbers, startNum and endNum, as input . the function should return an array containing numbers and specific strings based on following rules.
+
+For each number i in the range from startNum to endNum (both inclusive)
+-id i is divisible by both 3 and 5, include "fizzbuzz" in the result.
+-if i is divisible by only 3, include "fizz" in the result.
+-if i is divisible by only 5, include "buzz" in the result.
+-otherwiese, include the number i itself.
+*/
