6d20c3faa320451e24864ccfc2d5d9ebba0f66c7
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-12-13 15:37:32 +0530

    add code

--- Dom/Task 1/script.js
+++ Dom/Task 1/script.js
@@ -1,2 +1,5 @@
-let head=document.getElementById("heading");
-head.style.color="red"
+let head = document.getElementById("heading");
+head.style.color = "red";
+
+let headContent = document.getElementById("head");
+headContent.innerHTML = "Hello";
